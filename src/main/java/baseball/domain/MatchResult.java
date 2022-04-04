1e5ab3857a9fe5b3f00f40fd73d74f1daf873ad1
lovethefeel <lovethefeel@gmail.com>
2022-04-05 00:26:32 +0900

    feat 베이스볼 야구 매치 결과 도메인 작성

---
+++ src/main/java/baseball/domain/MatchResult.java
@@ -0,0 +1,33 @@
+package baseball.domain;
+
+public class MatchResult {
+    public static final int THREE_STRIKE = 3;
+    public static final int ZERO = 0;
+    private int strike;
+    private int ball;
+
+    private MatchResult(final int strike, final int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public static MatchResult empty() {
+        return new MatchResult(ZERO, MatchResult.ZERO);
+    }
+
+    public static MatchResult of(final int strike, final int ball) {
+        return new MatchResult(strike, ball);
+    }
+
+    public int strikeCount() {
+        return this.strike;
+    }
+
+    public int ballCount() {
+        return this.ball;
+    }
+
+    public boolean isThreeStrike() {
+        return this.strike == THREE_STRIKE;
+    }
+}
