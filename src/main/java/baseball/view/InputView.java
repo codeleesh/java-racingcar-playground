3a11835252b51cc7f77b704beef2cedf3ade1afa
lovethefeel <lovethefeel@gmail.com>
2022-04-04 23:55:34 +0900

    feat 입력 메시지 출력 및 입력창 메소드 작성

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,20 @@
+package baseball.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputNumberPrint() {
+        System.out.println("숫자를 입력해주세요 : ");
+        return scanner.next();
+    }
+
+    public static String inputMenuNumberPrint() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return scanner.next();
+    }
+
+    private InputView() {}
+}
